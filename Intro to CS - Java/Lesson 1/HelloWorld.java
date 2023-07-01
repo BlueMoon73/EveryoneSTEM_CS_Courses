6266275d5bfe56b9edf136a601cfae39431a7d15
Monish Saravana Kumar Divya Sundari <74512799+BlueMoon73@users.noreply.github.com>
2023-07-01 18:50:43 -0400

    Added header Comment.
    Added header comment

--- Intro to CS - Java/Lesson 1/HelloWorld.java
+++ Intro to CS - Java/Lesson 1/HelloWorld.java
@@ -1,3 +1,15 @@
+//*******************************************************************
+// Author: Monish Saravana Kumar Divya Sundari
+// Course: Everyone STEM - Intro to CS: Java
+// Last Modified: 7/1/2023
+//
+// PURPOSE:
+// - Contains all the code mentioned in Lesson 1 of, Everyone STEM's Intro to CS: Java Course.
+
+// ASSUMPTIONS:
+// - Assumes that you have read through the associated slides for this lesson.
+//*******************************************************************
+
 class HelloWorld { 
   public static void main(String[] args) {
     System.out.println("Hello, World!"); 
