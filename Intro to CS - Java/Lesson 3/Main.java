2318d432cd63a785c722acf57990baf24c4ecd3c
Monish Saravana Kumar Divya Sundari <74512799+BlueMoon73@users.noreply.github.com>
2023-07-01 18:53:58 -0400

    Added header comment
    Added header comment and fixed inline comments

--- Intro to CS - Java/Lesson 3/Main.java
+++ Intro to CS - Java/Lesson 3/Main.java
@@ -1,14 +1,28 @@
+//*******************************************************************
+// Author: Monish Saravana Kumar Divya Sundari
+// Course: Everyone STEM - Intro to CS: Java
+// Last Modified: 7/1/2023
+//
+// PURPOSE:
+// - Contains all the code mentioned in Lesson 3 of, Everyone STEM's Intro to CS: Java Course.
+//
+// ASSUMPTIONS:
+// - Assumes that you have read through the associated slides for this lesson. 
+//*******************************************************************
+
 public class Main {
     public static void main(String[] args) {
-
-
+        
+        // ------------------------------- If Statements ------------------------------
+        
         int age = 16;
         // if the variable age is greater than or equal to 18, run the following code.
         if (age >= 18) {
             System.out.println("You are eligible to vote!");
         }
 
-
+        // ------------------------------- If-Else Statements ------------------------------
+        
         int number = 10;
         if (number % 2 == 0) {
             System.out.println("The number is even.");
@@ -16,6 +30,7 @@ public class Main {
             System.out.println("The number is odd.");
         }
 
+        // ------------------------------- Nested If Statements ------------------------------
 
         int num = 25;
         if (num > 0) {
