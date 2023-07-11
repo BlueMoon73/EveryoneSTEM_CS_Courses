e89e0d708998036d756c703747da06e21f389cf0
Monish Saravana <monish.sd.2008@Gmail.com>
2023-07-11 18:16:27 -0400

    Created solution code for Lesson 1 Activity

---
+++ Intro to CS - Java/Lesson 1/Lesson1Solutions.java
@@ -0,0 +1,27 @@
+//*******************************************************************
+// Author: Monish Saravana Kumar Divya Sundari
+// Course: Everyone STEM - Intro to CS: Java
+// Last Modified: 7/11/2023
+//
+// PURPOSE:
+// - Contains all the code mentioned in Lesson 9 of, Everyone STEM's Intro to CS: Java Course.
+//
+// ASSUMPTIONS:
+// - Assumes that you have read through the associated slides for this lesson.
+//*******************************************************************
+
+public class Lesson1Solutions {
+    // ------------------------------- Activity Code  -------------------------------
+    public static void main(String[] args) {
+        System.out.println("My name is Monish! I'm 15 years old, and fun fact, I have 4 last names!");
+
+        // ------------------------------- Challenge Code  -------------------------------
+        // each System.out.println() Statement prints onto a new line!
+        System.out.println("My name Monish!");
+        System.out.println("I'm 15 years old.");
+        System.out.println("Fun fact, I have 4 last names!");
+
+    }
+
+
+}
