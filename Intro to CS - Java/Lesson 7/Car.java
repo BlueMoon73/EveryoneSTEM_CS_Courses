da640a2e2987980747152c687a9d9a94d448cf0e
Monish Saravana <monish.sd.2008@Gmail.com>
2023-07-14 19:04:13 -0400

    Updated car.java

--- Intro to CS - Java/Lesson 7/Car.java
+++ Intro to CS - Java/Lesson 7/Car.java
@@ -1,14 +1,16 @@
 public class Car {
     // Private field
-    private String name;
     private int releaseYear = 2001;
 
     // Public methods for accessing and modifying the private field
     public String getName() {
         return name;
     }
-    public void setName(String newName) {
-        name = newName;
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     // ------------------------------- The Static Keyword -------------------------------
